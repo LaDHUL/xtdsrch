baaa93b31822437c5922f16e87d30104bc473886
Loïc Jaouen <loic.jaouen@unil.ch>
2018-10-18 08:21:10 +0200

    adding the two basic data objects

---
+++ xtdsrch/src/app/person.ts
@@ -0,0 +1,9 @@
+// Data Person
+// one of the two basic objects we deal with
+export class Person {
+  id: string;
+  firstName: string;
+  lastName: string;
+  birth: string;
+  death: string;
+}
---
+++ xtdsrch/src/app/work.ts
@@ -0,0 +1,9 @@
+// Data Work
+// one of the two basic objects we deal with
+export class Work {
+  title: string;
+  format: string;
+  kind: string;
+  publication: string;
+  author: string;
+}

da2ca41e41c9b21fd5068e309fcf8ab8eb7662f9
Loïc Jaouen <loic.jaouen@unil.ch>
2018-10-19 10:24:47 +0200

    routes on main page

--- xtdsrch/src/app/app.module.ts
+++ xtdsrch/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
-import { PersonComponent } from './person/person.component';
+import { AppRoutingModule }     from './app-routing.module';
+import { PersonsComponent } from './persons/persons.component';
+import { WorksComponent } from './works/works.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonComponent
+    PersonsComponent,
+    WorksComponent,
+    SearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
